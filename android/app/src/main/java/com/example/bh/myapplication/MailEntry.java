5de335bbaaa99fdaf94df33428b8660066f51c0d
rlarlvy153 <rlarlvy153@naver.com>
2017-07-10 18:28:28 +0900

    안드로이드 코드 추가

---
+++ android/app/src/main/java/com/example/bh/myapplication/MailEntry.java
@@ -0,0 +1,70 @@
+package com.example.bh.myapplication;
+
+/**
+ * Created by BH on 2017-07-10.
+ */
+
+public class MailEntry
+{
+    private int mailId;
+    private String subject;
+    private String sender;
+    private String receiver;
+    private String date;
+    private String innerText;
+    public MailEntry(int id, String sub,String sen, String rec, String date,String text){
+        mailId = id;
+        subject = sub;
+        sender = sen;
+        receiver = rec;
+        this.date = date;
+        innerText = text;
+    }
+    public int getMailId(){
+        return mailId;
+    }
+
+    public void setMailId(int mailId) {
+        this.mailId = mailId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getInnerText() {
+        return innerText;
+    }
+
+    public void setInnerText(String innerText) {
+        this.innerText = innerText;
+    }
+}
