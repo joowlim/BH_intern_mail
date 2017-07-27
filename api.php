261fc979b50a942bf16389e68b53dd9236086921
joowlim <katejlim@gmail.com>
2017-07-27 16:52:03 +0900

    api.php 추가

---
+++ api.php
@@ -0,0 +1,48 @@
+<?php
+// Get request number
+$request = trim($_SERVER['PATH_INFO']);
+$request = str_replace("/", "", $request);
+
+// Get Method
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Connect to the db
+$link = mysqli_connect('localhost', 'root', 'root', 'intern');
+mysqli_set_charset($link, 'utf8');
+
+switch ($method)
+{
+  case 'GET':
+    $sql = "SELECT DISTINCT title, inner_text, mail_date, sender, receiver " .
+           "FROM mail, mail_log " .
+           "WHERE mail.mail_id = mail_log.mail_id AND receiver = '$request' " .
+           "ORDER BY mail.mail_date DESC LIMIT 10";
+    break;
+  case 'POST':
+    break;
+}
+
+// Execute sql
+$result = mysqli_query($link, $sql);
+
+if(!$result)
+{
+  // Return error
+  http_response_code(404);
+  die(mysqli_error($link));
+}
+
+if($method == 'GET')
+{
+  echo '[';
+  for($i = 0; $i < mysqli_num_rows($result); $i++)
+  {
+    echo ($i > 0 ? ',' : '').json_encode(mysqli_fetch_object($result));
+  }
+  echo ']';
+}
+
+// Close connection
+mysqli_close($link);
+?>
+
