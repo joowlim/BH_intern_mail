510246eae83841f422df5da6b2d4bb4e8177748c
joowlim <katejlim@gmail.com>
2017-08-02 13:54:01 +0900

    add exception handling

--- api.php
+++ api.php
@@ -26,7 +26,10 @@ while(!feof($user_config)) {
 }
 fclose($user_config);
 $mail_list = $_GET['mail_list'];
-
+if(count($mail_list) == 0){
+	echo '사용법 : api.php/mail_list[]=메일1&mail_list[]=메일2...로 호출해 주세요';
+	return;
+}
 $inner_sql = "";
 
 for($i = 0; $i< count($mail_list); $i++){
